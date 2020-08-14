5c84f264820d7d96a592d4a78db70ff47e216f52
faye wong <trandaosimanh@gmail.com>
2020-08-14 14:56:30 +0700

    cc

--- client/config/config.js
+++ client/config/config.js
@@ -1,8 +1,8 @@
-export const IP = "http://192.168.0.101"; // Wifi nha
+// export const IP = "http://192.168.0.101"; // Wifi nha
 // export const IP = "http://10.183.23.213";
 // export const IP = "http://172.20.10.14";
 // export const IP = "http://192.168.51.6";
-// export const IP = "http://192.168.43.51"; // Wifi dien thoai
+export const IP = "http://192.168.43.51"; // Wifi dien thoai
 // export const IP = "http://192.168.1.5";
 // export const IP = "http://localhost";
 // export const IP = "http://10.204.227.213"
